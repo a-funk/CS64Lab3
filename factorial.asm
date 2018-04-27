1590bbe51e618413cc9b40496e3fa04bd674c6eb
Erez Hajaj <erezhajaj@gmail.com>
2018-04-27 15:39:26 -0700

    completed factorial

--- factorial.asm
+++ factorial.asm
@@ -24,9 +24,37 @@ newline:
 .text
 
 main:
-
 	# TODO: Write your code here
+	# Print prompt
+	li $v0, 4
+	la $a0, prompt
+	syscall
 
+	#Get Input
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	li $s0, 1
+fact:
+	beq $t0, $zero, exit_loop
+	mult $s0, $t0
+	mflo $s0
+
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	li $v0, 4
+	la $a0, newline
+	syscall
+	sub $t0, $t0, 1
+	j fact
+exit_loop:
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	li $v0, 4
+	la $a0, newline
+	syscall
 exit:
 
 	# Exit
